c7e404bc5d5bd1eac7ff768dcfe3d3fd0a6c9b76
octavioben <smurfbenmed@gmail.com>
2024-02-05 12:29:06 -0500

    added reporter, workflow modified

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     specPattern: 'cypress/tests/**/*.cy.js',
   },
+  video: true,
+  screenshotsFolder: "cypress/screenshots",
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  }
 });
